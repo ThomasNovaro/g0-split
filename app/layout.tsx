7dddd649fa61b8a3b4b8417392de74f91c36988c
Thomas Novaro <thomas.novaro.dev@gmail.com>
2024-02-13 10:58:12 +0100

    Update description in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "G0 Split",
-  description: "Split every expenses, with everyone, on the G0",
+  description: "Split every expenses, with anyone, on the G0",
 }
 
 export default function RootLayout({
