f2853df93bd7b872cade020786a553aadecfb1f3
Thomas Novaro <thomas.novaro.dev@gmail.com>
2024-02-13 15:14:18 +0100

    Add favicon and app icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,26 @@ export default function RootLayout({
             gtag('config', 'G-ZF2T3R39MW');
           `}
         </Script>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="public/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="public/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="public/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="public/icons/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c"></meta>
+        <meta name="theme-color" content="#ffffff"></meta>
       </head>
       <body className={GeistSans.className}>{children}</body>
     </html>
